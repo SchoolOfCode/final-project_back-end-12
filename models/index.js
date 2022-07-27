b5d367494b9f4fdce4ba3ad40a3aff93c43a24b8
fiona <fiona.kitchen@yahoo.co.uk>
2022-07-27 11:36:55 +0100

    checking version are the same

--- models/index.js
+++ models/index.js
@@ -6,8 +6,9 @@ export async function getAll() {
   return res.rows;
 }
 export async function getByItem(item) {
+  console.log('im starting to get by item ')
   let res = await query(
-    `SELECT * FROM produce WHERE lower(name) LIKE lower($1) RETURNING *`,
+    `SELECT * FROM produce WHERE lower(name) LIKE lower($1)`,
     [item]
   );
   return res.rows;
