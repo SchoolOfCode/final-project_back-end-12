b16e1178dac83b69c844d4bdba5f6e69faea5d6b
fiona <fiona.kitchen@yahoo.co.uk>
2022-07-27 10:36:32 +0100

    troubleshooting produce paths

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,11 @@ import { getAll, getByItem } from "../models/index.js";
 const router = express.Router();
 
 router.get("/", async function (req, res) {
-  console.log(req.query);
+/*   console.log(req.query);
   if (req.query !== undefined) {
     let result = await getByItem(req.query);
     return res.json({ success: true, payload: result });
-  }
+  } */
   console.log(`Getting all items`);
   let result = await getAll();
   return res.json({ success: true, payload: result });
