0ab4c18e4bc518222b2d46ae014470b3af3fa386
fiona <fiona.kitchen@yahoo.co.uk>
2022-07-27 10:40:11 +0100

    adding item search

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,11 @@ import { getAll, getByItem } from "../models/index.js";
 const router = express.Router();
 
 router.get("/", async function (req, res) {
-/*   console.log(req.query);
-  if (req.query !== undefined) {
-    let result = await getByItem(req.query);
+  console.log(req.query);
+  if (req.query.item !== undefined) {
+    let result = await getByItem(req.query.item);
     return res.json({ success: true, payload: result });
-  } */
+  }
   console.log(`Getting all items`);
   let result = await getAll();
   return res.json({ success: true, payload: result });
