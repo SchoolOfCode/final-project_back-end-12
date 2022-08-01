8d3dca4b1bb4f79876c0f297740e608a34981dff
multi-vit <103585624+multi-vit@users.noreply.github.com>
2022-08-01 12:29:45 +0100

    Account for random number being the same
    Co-authored-by: Matt Miller <101735099+codedresser@users.noreply.github.com>

--- helper_scripts/randomGet.js
+++ helper_scripts/randomGet.js
@@ -1,14 +1,22 @@
 export function getRandom(allResults) {
   let randomResults = [];
   let length = allResults.length;
+
   if (length < 5) {
     return allResults;
   } else {
     for (let i = 0; i < 5; i++) {
-      console.log("Returning 5 items");
       let randomNumber = Math.floor(Math.random() * length);
-      console.log(randomNumber);
-      randomResults.push(allResults[randomNumber]);
+      if (
+        randomResults.some((item) => {
+          return item.id === allResults[randomNumber].id;
+        })
+      ) {
+        console.log("Duplicate found, going again!");
+        i--;
+      } else {
+        randomResults.push(allResults[randomNumber]);
+      }
     }
     return randomResults;
   }
