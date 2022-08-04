254030ed9b0ce4b5a627aa0f2bc7be2d13389b46
N-LEE-94 <nathanlee42@btinternet.com>
2022-08-04 16:09:48 +0100

    completed tests to ensure no duplicates are coming through the random function
    Co-authored-by: Matt Miller <101735099+codedresser@users.noreply.github.com>

--- helper_scripts/randomGet.test.js
+++ helper_scripts/randomGet.test.js
@@ -13,47 +13,18 @@ test("Testing the getRandom to check that it produces 5 objects from an array of
 
 });
 
-test("Testing the getRandom to check that it produces 5 unique objects with no duplicates from an array of 10 (with 5 duplicates)", () => {
-  
-  const data =  duplicateData;
-
-  const testArray = getRandom(data);
-
-  let randomResults = [];
-
-  let check = null;
-
-  function randomTest(){
-
-
-    for (let i = 0; i < 5; i++) {
-
-        if (
-          testArray.some((item) => {
-            return item.id === randomResults[i];
-          })
-        ) {
-          let check = false;
-        } else {
-          randomResults.push(allResults[randomNumber]);
-          let check = true;
-        }
-    }
-  } 
-
-  expect(check).toEqual(true);
-
-});
-
 // test("Testing the getRandom to check that it produces 5 unique objects with no duplicates from an array of 10 (with 5 duplicates)", () => {
   
 //   const data =  duplicateData;
 
 //   const testArray = getRandom(data);
 
-//   let randomResult = randomData;
+//   let randomResults = [];
 
-//   function randomTest(testArray, randomResults){
+//   let check = null;
+
+//   function randomTest(){
+
 
 //     for (let i = 0; i < 5; i++) {
 
@@ -62,46 +33,40 @@ test("Testing the getRandom to check that it produces 5 unique objects with no d
 //             return item.id === randomResults[i];
 //           })
 //         ) {
-//           false;
+//           check = false;
 //         } else {
-//           true;
+//           randomResults.push(allResults[randomNumber]);
+//           check = true;
 //         }
 //     }
 //   } 
 
-//   expect().toEqual();
+//   expect(check).toEqual(true);
 
 // });
 
-  //Plan
-  // expecting the sum of all testArray.id = (57, 58, 59, 60, 61)Sum of them = 295
-  // create variable for id sum =0
-  // To get the sum we will loop through (.map) and add id integer to id sum.
-  // test the idsum equals what we are expecting (295).
-
-
-
-
-// test("Testing the getRandom to check that it produces 5 objects from an array of 10", () => {
-//   const data =  FirstTenData;
+test("Testing the getRandom to check that it produces 5 unique objects with no duplicates from an array of 10 (with 5 duplicates)", () => {
   
-//   expect(getRandom(data)).toEqual(array.length === 5);
-//   expect(response.body).toEqual(
-//     expect.objectContaining({
-//       success: true,
-//       payload: expect.arrayContaining([
-//         expect.objectContaining({
-//           id: expect.any(Number),
-//           name: "Pear",
-//           imageurl: expect.any(String),
-//           description: expect.any(String),
-//           family: expect.any(String),
-//           foodtype: expect.any(String),
-//           usedas: expect.any(String),
-//           month: expect.arrayContaining([expect.any(String)]),
-//           allergens: expect.any(String),
-//         }),
-//       ]),
-//     })
-//   );
-// });
+  const data =  duplicateData;
+  const result = getRandom(data);
+
+  let fiftySeven = 0;
+  let fiftyEight = 0;
+  let fiftyNine = 0;
+  let sixty = 0;
+  let sixtyOne = 0;
+
+  for (let i = 0; i < result.length; i++) {
+    if (result[i].id === 57) { fiftySeven ++}
+    else if (result[i].id === 58) { fiftyEight ++}
+    else if (result[i].id === 59) { fiftyNine ++}
+    else if (result[i].id === 60) { sixty ++}
+    else if (result[i].id === 61) { sixtyOne ++}
+    }
+
+  expect(fiftySeven).toEqual(1);
+  expect(fiftyEight).toEqual(1);
+  expect(fiftyNine).toEqual(1);
+  expect(sixty).toEqual(1);
+  expect(sixtyOne).toEqual(1);
+});
