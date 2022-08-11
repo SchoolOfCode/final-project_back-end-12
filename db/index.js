c6d797b76e26f1886935160375b6de2f87303d26
fiona <fiona.kitchen@yahoo.co.uk>
2022-08-11 10:06:12 +0100

    expanded testing strings to give more context and added a small note on the get five function

--- db/index.js
+++ db/index.js
@@ -12,5 +12,6 @@ export default function query(text, params, callback) {
   return pool.query(text, params, callback);
 }
 
-// Checks that query is a-ok.
+// This is our connection to the database- check your PGURI is up to date in the .env variables 
+//below is a console.log that tests you've set up your connecction - by running dbtest the current time will be printed in the console
 // console.log(await query("SELECT NOW()"))
--- models/index.test.js
+++ models/index.test.js
@@ -2,7 +2,7 @@ import{ pool } from '../db/index.js'
 import { getAll, getByItem, getByMonth } from "./index.js";
 import { test, expect, describe } from "@jest/globals";
 
-test('testing the getAll model retrieves all ', async () => {
+test('testing the getAll model retrieves all items from the database', async () => {
   const response= await getAll();
   expect(response).toEqual(expect.any(Array));  
   expect(response).toEqual(expect.arrayContaining([expect.any(Object)]));
@@ -10,12 +10,12 @@ test('testing the getAll model retrieves all ', async () => {
 
 })
 
-test('testing for the get by item function', async() => {
+test('testing for the get by item function to make sure it returns the item selected', async() => {
 const response= await getByItem('pear')
 expect(response).toEqual(expect.arrayContaining([expect.objectContaining({id:expect.any(Number),name:'Pear',imageurl:expect.any(String),description:expect.any(String),family:expect.any(String),foodtype:expect.any(String),usedas:expect.any(String),month:expect.arrayContaining([expect.any(String)]),allergens:expect.any(String)})]));
 })
 
-test('testing for the get by a month function ', async() => {
+test('testing for the get by a month function to make sure it returns an item expected ', async() => {
     const response= await getByMonth('march')
     expect(response).toEqual(expect.arrayContaining([expect.objectContaining({id:expect.any(Number),name:expect.any(String),imageurl:expect.any(String),description:expect.any(String),family:expect.any(String),foodtype:expect.any(String),usedas:expect.any(String),month:expect.arrayContaining(['March']),allergens:expect.any(String)})]));
     })
